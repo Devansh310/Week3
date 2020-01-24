7c8187e9f9dd9d90afd108ba4ac688505471abdc
Devansh310 <60017685+Devansh310@users.noreply.github.com>
2020-01-24 15:29:52 -0500

    Update Gitexample.java

--- src/gitexample/Gitexample.java
+++ src/gitexample/Gitexample.java
@@ -17,6 +17,7 @@ public class Gitexample {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("This is an example");
+        //date 24/1/2020
     }
     
 }
